ade01e6da09fc5c6bed29f1ec3fedced46c5ed3f
ggl-09 <166072344+ggl-09@users.noreply.github.com>
2024-05-06 21:55:53 +0000

    6/05; 23:55h

--- parelles.cpp
+++ parelles.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+bool es_vocal(char x) {
+  if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u') return true; 
+  else return false;
+}
+
+int parelles(char vec[], int n_max, int n) {
+  int n_parelles = 0;
+  if (n >= n_max) return 0;
+  if (es_vocal(vec[n]) and es_vocal(vec[n+1])) return 1 + parelles(vec, n_max, n+1);
+  else return parelles(vec, n_max, n+1);
+}
+
 int main() {
+  int n_car;
+  do {
+    cout << "ENTRA EL NOMBRE DE CARACTERS: " << endl;
+    cin >> n_car;
+  } while (n_car <= 0);
+  char vec[n_car];
+  char ll;
+  cout << "ENTRA ELS CARACTERS: " << endl;
+  for (int i = 0; i < n_car; i++) {
+    cin >> vec[i];
+  }
+  cout << "NOMBRE DE PARELLES DE VOCALS: " << parelles(vec,n_car,0);
   return 0;
 }

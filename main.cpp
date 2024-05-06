df85d749571b076bfb0181a1f84b29f6c3a8eaf4
ggl-09 <166072344+ggl-09@users.noreply.github.com>
2024-05-06 22:09:15 +0000

    7/05; 00:09h

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+int bad_luck(long long number, int digit) {
+  if (number == 0) return 0;
+  int ultim = number % 10;
+  if (ultim == digit) return bad_luck(number / 10, digit);
+  return bad_luck(number / 10, digit) * 10 + ultim;
+}
+
 int main() {
+  long long number;
+  int digit;
+  do {
+    cout << "ENTRA UN ENTER NO NEGATIU:" << endl;
+    cin >> number;
+  } while (number < 0);
+  do {
+    cout << "ENTRA EL DIGIT DE LA MALA SORT:" << endl;
+    cin >> digit;
+  } while(digit > 9 or digit < 0);
+  cout << "VALOR FINAL:" << endl << bad_luck(number, digit) << endl;
   return 0;
 }
